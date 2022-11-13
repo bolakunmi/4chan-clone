2613bd94a1f61251f2bc605788c7decc4bbe173d
Bolakunmi <boluwayemi2000@gmail.com>
2022-11-13 17:12:58 +0100

    more work done, media query left and more styling

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import "./App.css";
 
 function App() {
   const [descriptionDisplay, setDescriptionDisplay] = useState(true);
-  console.log(descriptionDisplay);
+
   return (
     <div className="general">
       <img
         alt="logo"
         src={require("./media/logo-transparent.png")}
         className="logo"
+        href="/"
       />
 
       {descriptionDisplay && (
         <div className="description">
           <div className="menu_bar">
-            <div>what is 4chan?</div>
+            <h4>what is 4chan?</h4>
             <button
               type="button"
               className="close-button"
@@ -47,16 +48,139 @@ function App() {
 
       <div className="menus">
         <div className="menu_bar">
-          <div>Boards</div>
-          <div>filter ▼</div>
+          <h4>Boards</h4>
+          <div className="dropdown">
+            <div className="dopdown-button">filter ▼</div>
+            <div className="dropdown-content">
+              <div><a href="/">Show All Boards</a></div>
+              <div><a href="/">Show NSFW Boards only</a></div>
+              <div><a href="/">Show Worksafe Boards Only</a></div>
+              <div><a href="/">Show All Boards (Classic)</a></div>
+              <hr></hr>
+              <div><a href="/">Use Frames</a></div>
+              <div><a href="/">Use Catalog</a></div>
+            </div>
+          </div>
         </div>
-        <div>post pictures and links</div>
+        <div className="Boards-content">
+          <div>
+            <h6>Japanese Culture</h6>
+            <div><a href="/">Anime & Manga</a></div>
+            <div><a href="/">Anime/Cute</a></div>
+            <div><a href="/">Anime/Wallpaper</a>s</div>
+            <div><a href="/">Mecha</a></div>
+            <div><a href="/">Cosplay & EGL</a></div>
+            <div><a href="/">Cute/Male</a></div>
+            <div><a href="/">Flas</a>h</div>
+            <div><a href="/">Transportation</a></div>
+            <div><a href="/">Otaku Culture</a></div>
+            <div><a href="/">Virtual YouTubers</a></div>
+          </div>
+
+          <div>
+            <h6>Video Games</h6>
+            <div><a href="/">Video Games</a></div>
+            <div><a href="/">Video Games General</a></div>
+            <div><a href="/">Video Games/ Multilayer</a></div>
+            <div><a href="/">Video Games/ Mobile</a></div>
+            <div><a href="/">Naruto</a></div>
+            <div><a href="/">Retro Games</a></div>
+            <div><a href="/">Video games/RPG</a></div>
+            <div><a href="/">Video games/Strategy</a></div>
+          </div>
+
+          <div>
+            <h6>Interests</h6>
+            <div><a href="/">Comics & Cartoons</a></div>
+            <div><a href="/">Technology</a></div>
+            <div><a href="/">Television & Film</a></div>
+            <div><a href="/">Weapons</a></div>
+            <div><a href="/">Auto</a></div>
+            <div><a href="/">Animals & Nature</a></div>
+            <div><a href="/">Traditional Games</a></div>
+            <div><a href="/">Sports</a></div>
+            <div><a href="/">Extreme Sports</a></div>
+            <div><a href="/">Professional Wrestling</a></div>
+            <div><a href="/">Science & Math</a></div>
+            <div><a href="/">History & Humanities</a></div>
+            <div><a href="/">International</a></div>
+            <div><a href="/">Outdoors</a></div>
+            <div><a href="/">Toys</a></div>
+          </div>
+
+          <div>
+            <h6>Creative</h6>
+            <div><a href="/">Oekaki</a></div>
+            <div><a href="/">Papercraft & Origami</a></div>
+            <div><a href="/">Photography</a></div>
+            <div><a href="/">Food & Cooking</a></div>
+            <div><a href="/">Artwork/Critique</a></div>
+            <div><a href="/">Wallpapers/General</a></div>
+            <div><a href="/">Literature</a></div>
+            <div><a href="/">Music</a></div>
+            <div><a href="/">Fashion</a></div>
+            <div><a href="/">3DCG</a></div>
+            <div><a href="/">Graphic Design</a></div>
+            <div><a href="/">Do-It-Yourself</a></div>
+            <div><a href="/">Worksafe GIF</a></div>
+            <div><a href="/">Quests</a></div>
+          </div>
+
+          <div>
+            <h6>Other</h6>
+            <div><a href="/">Business & Finance</a></div>
+            <div><a href="/">Travel</a></div>
+            <div><a href="/">Fitness</a></div>
+            <div><a href="/">Paranormal</a></div>
+            <div><a href="/">Advice</a></div>
+            <div><a href="/">LGBT</a></div>
+            <div><a href="/">Pony</a></div>
+            <div><a href="/">Current News</a></div>
+            <div><a href="/">Worksafe Requests</a></div>
+            <div><a href="/">Very Important Posts</a></div>
+          </div>
+
+          <div>
+            <h6>Misc.</h6>
+            <div><a href="/">Random</a></div>
+            <div><a href="/">ROBOT9001</a></div>
+            <div><a href="/">Politically Incorrect</a></div>
+            <div><a href="/">International/Random</a></div>
+            <div><a href="/">Cams & Meetups</a></div>
+            <div><a href="/">Shit 4chan Says</a></div>
+          </div>
+
+          <div>
+            <h6>Adults</h6>
+            <div><a href="/">Sexy Beautiful Women</a></div>
+            <div><a href="/">Hardcore</a></div>
+            <div><a href="/">Handsome Men</a></div>
+            <div><a href="/">Hentai</a></div>
+            <div><a href="/">Ecchi</a></div>
+            <div><a href="/">Yuri</a></div>
+            <div><a href="/">Hentai/Alternative</a></div>
+            <div><a href="/">Yaoi</a></div>
+            <div><a href="/">Torrents</a></div>
+            <div><a href="/">High Resolution</a> </div>
+            <div><a href="/">Adult GIF</a></div>
+            <div><a href="/">Adult Cartoons</a></div>
+            <div><a href="/">Adult Requests</a></div>
+          </div>
+        </div>
       </div>
       <br></br>
       <div className="menus">
         <div className="menu_bar">
-          <div>Popular Threads</div>
-          <div>options ▼</div>
+          <h4>Popular Threads</h4>
+
+          <div className="dropdown">
+            <div className="dopdown-button">options ▼</div>
+            <div className="dropdown-content">
+              <div>Show Worksafe Content Only</div>
+              <div>Show NSFW Content only</div>
+              <div>Show All Content</div>
+            </div>
+          </div>
         </div>
         <div>post pictures and links</div>
       </div>
@@ -64,7 +188,7 @@ function App() {
       <br></br>
       <div className="menus">
         <div className="menu_bar">
-          <div>Stats</div>
+          <h4>Stats</h4>
         </div>
         <div className="stats">
           <span>Total Posts: 4,292,172,453</span>
@@ -77,45 +201,52 @@ function App() {
       <div className=" menus-more">
         <div className="more more-options">
           <span>
-            <a href={require("./#")}>Home</a>
+            <a href="/">Home</a>
           </span>
           <span>
-            <a href={require("./#")}>News</a>
+            <a href="/">News</a>
           </span>
           <span>
-            <a href={require("./#")}>Blog</a>
+            <a href="/">Blog</a>
           </span>
           <span>
-            <a href={require("./#")}>FAQ</a>
+            <a href="/">FAQ</a>
           </span>
           <span>
-            <a href={require("./#")}>Rules</a>
+            <a href="/">Rules</a>
           </span>
           <span>
-            <a href={require("./#")}>Support 4chan</a>
+            <a href="/">Support 4chan</a>
           </span>
           <span>
-            <a href={require("./#")}>Advertise</a>
+            <a href="/">Advertise</a>
           </span>
           <span>
-            <a href={require("./#")}>Press</a>
+            <a href="/">Press</a>
           </span>
           <span>
-            <a href={require("./#")}>日本語</a>
+            <a href="/">日本語</a>
           </span>
         </div>
       </div>
 
       <br></br>
       <div className="menus-more2">
-        <span><a href={require("./#")}>About</a></span>
+        <span>
+          <a href="/">About</a>
+        </span>
         <span>•</span>
-        <span><a href={require("./#")}>Feedback</a></span>
+        <span>
+          <a href="/">Feedback</a>
+        </span>
         <span>•</span>
-        <span><a href={require("./#")}>Legal</a></span>
+        <span>
+          <a href="/">Legal</a>
+        </span>
         <span>•</span>
-        <span><a href={require("./#")}>Contact</a></span>
-
+        <span>
+          <a href="/">Contact</a>
+        </span>
       </div>
       <br></br>
       <div className="copyright">
